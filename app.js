f4988a2c1d7539875935cf2b29fd3cebc5292a47
Abdalrahman Amr <abdalrahman.amr@baianat.net>
2025-05-29 13:02:15 +0300

    Sending Responses

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const http  = require('http');
 const server=http.createServer((req,res)=>{
 console.log(req.url,req.method,req.headers);
 //process.exit();  you don't want to exit ur server !!
+res.setHeader('Content-Type' , 'text/html') ; 
+res.write('<html>');
+res.write('<head><title>My first page</title></head>')
+res.write('<body><h1>hello node</h1></body>')
+res.write('</html>');
+res.end();
 
 });
 
