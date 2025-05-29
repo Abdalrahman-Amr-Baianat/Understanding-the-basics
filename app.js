09b9d7dfdfa4e887940ed884874b6f4a6d2ce18e
Abdalrahman Amr <abdalrahman.amr@baianat.net>
2025-05-29 15:56:59 +0300

    Parsing Requist Bodies

--- app.js
+++ app.js
@@ -5,22 +5,33 @@ const url = req.url;
 const method = req.method;
 
 if (url === "/") {
-res.setHeader('Content-Type', 'text/html');
-res.write('<html>');
-res.write('<head><title>Enter message</title></head>');
-res.write('<body><form action="/message" method="POST">');
-res.write('<input type="text" name="message">');
-res.write('<button type="submit">Send</button>');
-res.write('</form></body>');
-res.write('</html>');
-return res.end();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Enter message</title></head>');
+    res.write('<body><form action="/message" method="POST">');
+    res.write('<input type="text" name="message">');
+    res.write('<button type="submit">Send</button>');
+    res.write('</form></body>');
+    res.write('</html>');
+    return res.end();
 }
 
 if (url === '/message' && method === "POST"){
-fs.writeFileSync('message.txt' , 'Dummy')
-res.statusCode=302;
-res.setHeader('Location' , '/')
-return res.end()
+    const body = []
+    req.on('data' , (chunk)=> {
+    console.log(chunk);
+    body.push(chunk)
+    });
+    req.on('end',()=> {
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split("=")[1];
+        fs.writeFileSync("message.txt",message)
+    })
+
+    
+    res.statusCode=302;
+    res.setHeader('Location' , '/')
+    return res.end()
 }
 
 res.setHeader('Content-Type', 'text/html');
